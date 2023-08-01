64d273f45e1a977f202c84639e8dc792553494e2
ssdar <ssdar@10.175.6.142>
2023-08-01 14:07:25 +0800

    edit Transaction class

--- C206_CaseStudy/src/Transaction.java
+++ C206_CaseStudy/src/Transaction.java
@@ -1,4 +1,22 @@
 
-public class Transaction {
+public class Transaction extends User {
+	private String tDate;
+	private int amount;
+	private String desiredCurrency;
+	public Transaction(String username, String userID, String password, String contact, String role,String tDate, int amount, String desiredCurrency) {
+		super(username,userID,password,contact,role);
+		this.tDate=tDate;
+		this.amount=amount;
+		this.desiredCurrency=desiredCurrency;
+	}
+	public String tDate() {
+		return tDate;
+	}
+	public int amount() {
+		return amount;
+	}
+	public String desiredCurrency() {
+		return desiredCurrency;
+	}
 
 }
