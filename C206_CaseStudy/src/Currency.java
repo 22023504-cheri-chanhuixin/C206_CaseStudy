560ff315458113537dacc83f115c80893745802f
hzhi2 <hzhi2@husna>
2023-08-05 14:58:08 +0800

    Updated Currency Class (changed Symbol to Code)

--- C206_CaseStudy/src/Currency.java
+++ C206_CaseStudy/src/Currency.java
@@ -1,47 +1,26 @@
-import java.util.ArrayList;
-
 public class Currency {
-	private static String symbol;
-	private String name;
+    private String code;
+    private String name;
 
-	public Currency(String currency, String name) {
-		this.symbol = symbol;
-		this.name = name;
+    public Currency(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 
-	}
+    // Getters and Setters
+    public String getCode() {
+        return code;
+    }
 
-	public static String getSymbol() {
-		return symbol;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public String toString() {
-		String output = super.toString();
-		output = String.format("%-63s %-20s", output, name, symbol);
-		return output;
-	}
-
-	public static void viewAllCurrency(ArrayList<Currency> currencyList) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static void setHeader(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setIsAvailable(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean getIsAvailable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 }
