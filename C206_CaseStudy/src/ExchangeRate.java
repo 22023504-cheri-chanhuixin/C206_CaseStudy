907e9d7225237dfd14b4c58a9ddc041b87e5ea79
RAZIN ZHARFAN B MOHAMMED FIROOZ RP <21011556@myrp.edu.sg>
2023-08-06 16:46:05 +0800

    Update er

--- C206_CaseStudy/src/ExchangeRate.java
+++ C206_CaseStudy/src/ExchangeRate.java
@@ -6,29 +6,11 @@ public class ExchangeRate extends Currency {
         this.exchangeRate = exchangeRate;
     }
 
-    public double getExchangeRate() {
+  public double getExchangeRate() {
         return exchangeRate;
     }
 
     public void setExchangeRate(double exchangeRate) {
         this.exchangeRate = exchangeRate;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + " Exchange Rate: " + exchangeRate;
-    }
-
-    // Sample main method to create and display three currencies with exchange rates
-    public static void main(String[] args) {
-        // Create three sample currencies with exchange rates
-        ExchangeRate usdCurrency = new ExchangeRate("USD", "United States Dollar", 1.0);
-        ExchangeRate eurCurrency = new ExchangeRate("EUR", "Euro", 0.85);
-        ExchangeRate sgdCurrency = new ExchangeRate("SGD", "Singapore Dollar", 1.35);
-
-        // Display the details of the currencies with exchange rates
-        System.out.println(usdCurrency);
-        System.out.println(eurCurrency);
-        System.out.println(sgdCurrency);
     }
 }
